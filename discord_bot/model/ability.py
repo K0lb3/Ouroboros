0fb85743f5081f624132b4008a0c65d1ac1c31fb
Peter Han <git@peterhan.me>
2018-06-13 04:14:32 -0700

    Implement Gear and Ability models

---
+++ discord_bot/model/ability.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+from .model import Model
+
+
+class Ability(Model):
+    _LINK_FORMAT = "http://www.alchemistcodedb.com/skill/{iname}"
+
+    @property
+    def name(self):
+        return self._source.get('NAME')
+
+    @property
+    def expr(self):
+        return self._source.get('EXPR')
+
+    @property
+    def link(self):
+        return Ability._LINK_FORMAT.format(
+            iname=self.iname.lower().replace('_','-')
+        )
+
+    def __str__(self):
+        return '\n'.join([
+            "[{name}]({link})".format(name=self.name, link=self.link),
+            "Restriction: {rest}".format(rest=self.restriction),
+            self.expr,
+        ])
